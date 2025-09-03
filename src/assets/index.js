dd7d8e18d6a04410827927473d246236f9c098dc
Webprojon <tokhirjonkhasanov@gmail.com>
2025-09-03 10:20:57 +0200

    Add asset management for icons and images
    - Created a new index.js file to centralize imports for icons and images used across components.
    - Updated DesktopNavbar and MobileNavbar components to import assets from the new index file for improved organization.
    - Refactored VideoPlayer component to utilize centralized asset imports.
    - Adjusted VideoPage component to maintain correct imports for VideoPlayer and VideoDescription components.

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+// Desktop Navbar Items
+import menuIcon from "./icons/menu-icon.svg";
+import searchIcon from "./icons/search-icon.svg";
+import videoCameraIcon from "./icons/video-camera-icon.svg";
+import sixDotsIcon from "./icons/six-dots-icon.svg";
+import bellIcon from "./icons/bell-icon.svg";
+import logo from "./images/youtube-logo.png";
+import avatarImg from "./images/user-avatar.png";
+
+// Mobile Navbar Items
+import threeDotsIcon from "./icons/three-dots-icon.png";
+import homeIcon from "./icons/home-icon.svg";
+import trendingIcon from "./icons/trending-icon.svg";
+import subscriptionsIcon from "./icons/subscriptions-icon.svg";
+import libraryIcon from "./icons/library-icon.svg";
+
+// Video Description Items
+import channelAvatar from "./images/channel-avatar.png";
+
+// Video Player Items
+import videoCover from "./images/video-cover-main.png";
+import likeIcon from "./icons/like-icon.svg";
+import dislikeIcon from "./icons/dislike-icon.svg";
+import shareIcon from "./icons/share-icon.svg";
+import moreIcon from "./icons/more-icon.svg";
+import pauseIcon from "./icons/pause-icon.png";
+import nextIcon from "./icons/next-icon.png";
+import volumeIcon from "./icons/volume-icon.png";
+import volumeIcon2 from "./icons/volume.svg";
+import ccIcon from "./icons/subtitles-icon.png";
+import settingsIcon from "./icons/settings-icon.png";
+import sizeIcon from "./icons/size-icon.png";
+import fullscreenIcon from "./icons/full-screen-icon.png";
+
+// Export All Items
+export {
+	// Desktop Navbar Items
+	menuIcon,
+	searchIcon,
+	videoCameraIcon,
+	sixDotsIcon,
+	bellIcon,
+	logo,
+	avatarImg,
+
+	// Mobile Navbar Items
+	threeDotsIcon,
+	homeIcon,
+	trendingIcon,
+	subscriptionsIcon,
+	libraryIcon,
+
+	// Video Player Items
+	channelAvatar,
+
+	// Video Player Items
+	videoCover,
+	likeIcon,
+	dislikeIcon,
+	shareIcon,
+	moreIcon,
+	pauseIcon,
+	nextIcon,
+	volumeIcon,
+	volumeIcon2,
+	ccIcon,
+	settingsIcon,
+	sizeIcon,
+	fullscreenIcon,
+};
